1074f9339b1a2455e6505fb26f306ef0684bbd08
Clint <40567645+cakasten@users.noreply.github.com>
2023-06-01 11:01:12 +0200

    create navbar component

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import './navbar.css'
+
+const Navbar = (props) => {
+    return (
+        <div id='navbar'>
+            <p>dropdown</p>
+            <h1>Fake Reddit</h1>
+            <p>search</p>
+        </div>
+    )
+}
+
+export default Navbar;

40538281f316ff875081836a48fb3ee9d6497551
Clint <40567645+cakasten@users.noreply.github.com>
2023-06-05 11:55:57 +0200

    add reddit api fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
-import Navbar from './components/navbar/Navbar';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Post from "./components/post/Post";
 
 function App() {
+  const [postArray, setPostArray] = useState([]);
+
+  function getPosts() {
+    fetch("https://www.reddit.com/r/popular.json?limit=5")
+      .then((res) => res.json())
+      .then((data) => setPostArray(...postArray, data.data.children));
+  }
+
+  console.log(postArray)
+
+  useEffect(getPosts, []);
   return (
     <div className="App">
       <Navbar />
+      {postArray.map(post => <Post key={post.data.id} title={post.data.title} author={post.data.author} />)}
     </div>
   );
 }

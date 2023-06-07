f5d8548e049e20a762b433a6c38a790406b7cea3
Clint <40567645+cakasten@users.noreply.github.com>
2023-06-07 16:38:03 +0200

    add videos to posts

--- src/App.js
+++ src/App.js
@@ -7,18 +7,39 @@ function App() {
   const [postArray, setPostArray] = useState([]);
 
   function getPosts() {
-    fetch("https://www.reddit.com/r/popular.json?limit=5")
+    fetch("https://www.reddit.com/r/popular.json?limit=10")
       .then((res) => res.json())
       .then((data) => setPostArray(...postArray, data.data.children));
   }
 
-  console.log(postArray)
+  console.log(postArray);
 
   useEffect(getPosts, []);
   return (
     <div className="App">
       <Navbar />
-      {postArray.map(post => <Post key={post.data.id} title={post.data.title} author={post.data.author} body={post.data.selftext} votes={post.data.ups} />)} 
+      {postArray.map(
+        (post) => (
+          <Post
+            key={post.data.id}
+            title={post.data.title}
+            author={post.data.author}
+            votes={post.data.ups}
+            subreddit={post.data.subreddit}
+            video={post.data.is_video && post.data.media.reddit_video.fallback_url}
+          />
+        )
+        // : (
+        //   <Post
+        //     key={post.data.id}
+        //     title={post.data.title}
+        //     author={post.data.author}
+        //     votes={post.data.ups}
+        //     subreddit={post.data.subreddit}
+        //     thumbnail={post.data.thumbnail}
+        //   />
+        // )
+      )}
     </div>
   );
 }
--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -7,7 +7,10 @@ const Post = (props) => {
       <Votes votes={props.votes} />
       <h1 className={styles.title}>{props.title}</h1>
       <p className={styles.author}>Posted by: {props.author}</p>
-      <p className={styles.body}>{props.body}</p>
+      <p className={styles.subreddit}>r/{props.subreddit}</p>
+      {props.video ? <video className={styles.body} controls autoPlay width='100%' height='100%'>
+        <source src={props.video} type="video/mp4" />
+      </video> : <p className={styles.body}>No Video</p>}
     </div>
   );
 };

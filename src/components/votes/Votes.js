b807ce91398194c1a5a6e7fda8b9f4174dfd1830
Clint <40567645+cakasten@users.noreply.github.com>
2023-07-13 08:40:36 +0200

    give votes over 1000 decimal point

--- src/components/votes/Votes.js
+++ src/components/votes/Votes.js
@@ -1,12 +1,18 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleUp, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import styles from "./votes.module.css";
 
 const Votes = (props) => {
+  const { votes } = props;
+
+  const smallVotes =
+    votes > 1000 ? `${Math.round((votes / 1000) * 10) / 10} K` : votes;
+
   return (
     <div className={props.styles}>
-      <FontAwesomeIcon icon={faAngleUp} />
-      <p>{props.votes ? props.votes : "-"}</p>
-      <FontAwesomeIcon icon={faAngleDown} />
+      <FontAwesomeIcon icon={faAngleUp} className={styles.arrow} />
+      <p>{votes ? smallVotes : "-"}</p>
+      <FontAwesomeIcon icon={faAngleDown} className={styles.arrow} />
     </div>
   );
 };
